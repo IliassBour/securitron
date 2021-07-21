a8baccc93bf0105cf343957df44e1bd20eaa58bb
lb-udes <55243135+lb-udes@users.noreply.github.com>
2021-07-21 15:15:37 -0400

    Ajout des sources Vitis et modification du script TCL Zybo

---
+++ SauvegardeProjetAvecTCL/vitisProj/http_server_test/src/s4i_tools.h
@@ -0,0 +1,34 @@
+/*
+ * s4i_tools.h
+ *
+ *  Created on: 21 févr. 2020
+ *      Author: francoisferland
+ */
+
+#ifndef SRC_S4I_TOOLS_H_
+#define SRC_S4I_TOOLS_H_
+
+#define S4I_NUM_SWITCHES	4
+
+#include "xil_printf.h"
+#include <stdbool.h>
+
+void			s4i_init_hw();
+//int 			s4i_is_cmd_sws(char *buf);
+
+int				s4i_is_api_get(char *buff);
+int				s4i_is_api_full(char *buff);
+
+int				s4i_is_api_temp_max(char *buff);
+int				s4i_is_api_temp_min(char *buff);
+int				s4i_is_api_temp_avg(char *buff);
+
+int				s4i_is_api_sound_max(char *buff);
+int				s4i_is_api_sound_min(char *buff);
+int				s4i_is_api_sound_avg(char *buff);
+
+unsigned int 	s4i_get_sws_state();
+
+u8 AD1_GetSampleRaw(u8 address, bool first);
+
+#endif /* SRC_S4I_TOOLS_H_ */
